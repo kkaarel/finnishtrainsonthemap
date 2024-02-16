9ff3521c62de576f3485e14fff1bbc68c36514f2
kkaarel <informations@hotmail.com>
2024-02-16 15:37:09 +0200

    added spinner

--- trainsonmap.py
+++ trainsonmap.py
@@ -15,27 +15,30 @@ data = response.json()
 
 df = pd.json_normalize(data["features"])
 
-
 # Loop through the train numbers in the DataFrame and get the metadata for each train from the API
 metadata = []
-for train_number in df["properties.trainNumber"].unique():
-    metadata_url = f"https://rata.digitraffic.fi/api/v1/trains/latest/{train_number}"
-    metadata_response = requests.get(metadata_url)
-    metadata_data = metadata_response.json()
-    metadata_dict = metadata_data[0] if isinstance(metadata_data, list) and len(metadata_data) > 0 else {}
-    metadata.append({
-        "trainNumber": metadata_dict.get("trainNumber", ""),
-        "operatorShortCode": metadata_dict.get("operatorShortCode", ""),
-        "trainType": metadata_dict.get("trainType", ""),
-        "trainCategory": metadata_dict.get("trainCategory", "")
-    })
+with st.spinner('Fetching metadata...'):
+    count = 0
+    for train_number in df["properties.trainNumber"].unique():
+        metadata_url = f"https://rata.digitraffic.fi/api/v1/trains/latest/{train_number}"
+        metadata_response = requests.get(metadata_url)
+        metadata_data = metadata_response.json()
+        metadata_dict = metadata_data[0] if isinstance(metadata_data, list) and len(metadata_data) > 0 else {}
+        metadata.append({
+            "trainNumber": metadata_dict.get("trainNumber", ""),
+            "operatorShortCode": metadata_dict.get("operatorShortCode", ""),
+            "trainType": metadata_dict.get("trainType", ""),
+            "trainCategory": metadata_dict.get("trainCategory", "")
+        })
+        count += 1
+    st.text(f"Fetched metadata for {count} trains")
 
 # Create a DataFrame from the metadata
 metadata_df = pd.json_normalize(metadata)
 
-
 # Merge the metadata DataFrame with the train locations DataFrame
 merged_df = pd.merge(df, metadata_df, left_on="properties.trainNumber", right_on="trainNumber")
+
 
 # Create a map centered on the first geo point
 m = folium.Map(location=[merged_df.iloc[0]["geometry.coordinates"][1], merged_df.iloc[0]["geometry.coordinates"][0]], zoom_start=5)
